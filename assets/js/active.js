37ed17e428e7cab8b8c9f53b84d7d80c0db4bd65
dilshad-shibin <msdcva786@gmail.com>
2021-10-24 01:13:54 +0530

    index page added

---
+++ assets/js/active.js
@@ -0,0 +1,119 @@
+(function ($) {
+    'use strict';
+
+    var $window = $(window);
+    var zero = 0;
+
+
+    // :: 2.0 NAVIGATION MENU ACTIVE CODE
+    function navMenu() {
+
+        // MAIN MENU TOGGLER ICON (MOBILE SITE ONLY)
+        $('[data-toggle="navbarToggler"]').click(function () {
+            $('.navbar').toggleClass('active');
+            $('body').toggleClass('canvas-open');
+        });
+        // MAIN MENU TOGGLER ICON
+        $('.navbar-toggler').click(function () {
+            $('.navbar-toggler-icon').toggleClass('active');
+        });
+
+        // NAVBAR STICKY
+        var $stickyNav = $(".navbar-sticky");
+
+        $(window).on("scroll load", function () {
+            var scroll = $(window).scrollTop();
+            if (scroll >= 120) {
+                $stickyNav.addClass("navbar-sticky-moved-up");
+            } else {
+                $stickyNav.removeClass("navbar-sticky-moved-up");
+            }
+            // apply transition
+            if (scroll >= 250) {
+                $stickyNav.addClass("navbar-sticky-transitioned");
+            } else {
+                $stickyNav.removeClass("navbar-sticky-transitioned");
+            }
+            // sticky on
+            if (scroll >= 500) {
+                $stickyNav.addClass("navbar-sticky-on");
+            } else {
+                $stickyNav.removeClass("navbar-sticky-on");
+            }
+
+        });
+    }
+    navMenu();
+
+    // :: 3.0 SCROLL TO TOP ACTIVE CODE
+    var offset = 300;
+    var duration = 500;
+
+    $window.on('scroll', function () {
+        if ($(this).scrollTop() > offset) {
+            $("#scrollUp").fadeIn(duration);
+        } else {
+            $("#scrollUp").fadeOut(duration);
+        }
+    });
+
+    $("#scrollUp").on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, duration);
+    });
+
+    // :: 4.0 SCROLL LINK ACTIVE CODE
+    var scrollLink = $('.scroll');
+
+    // SCROLLSPY ACTIVE CODE
+    $('body').scrollspy({
+        target: '#sarah-header'
+    });
+
+    // :: 5.0 SMOOTH SCROLLING ACTIVE CODE
+    scrollLink.on('click', function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    });
+
+    // :: 7.0 AOS ACTIVE CODE
+    AOS.init();
+
+    // :: 8.0 PREVENT DEFAULT ACTIVE CODE
+    $("a[href='#']").on('click', function ($) {
+        $.preventDefault();
+    });
+
+
+
+    // :: 12.0 TEAM SLIDER ACTIVE CODE
+    $('.testi-slider.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: false,
+        dots: false,
+        smartSpeed: 2000,
+        autoplay: false,
+        autoplayTimeout: 4000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 1.7
+            }
+        }
+    });
+
+    
+
+}(jQuery));
